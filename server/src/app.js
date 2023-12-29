097bec59ab0cabba70a89109370bfe0e3de87e09
antoniogomes94 <antoniogomes313@outlook.com>
2023-12-29 15:58:40 -0300

    chore: criação do backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
-const exrpess = require("express")
-const cors = require("cors")
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/routes')
 
 require("dotenv").config()
 
@@ -8,5 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(routes)
 
 module.exports = app
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app")
-const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Server listener on port ${port}`)
-})
+const port = process.env.PORT || 5000
+
+app.listen(port, () =>
+  console.log(`Server listening on port ${port}`)
+)
